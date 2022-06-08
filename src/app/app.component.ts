947d5cb9c7bcc68de2bdde943a82979e10f538a1
chalermporn <bird.urights@gmail.com>
2022-06-08 16:28:12 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      name: [],
+      address: fb.array([]),
+    });
+  }
+
+  addNewAddressGroup() {
+    const add = this.form.get('address') as FormArray;
+    add.push(
+      this.fb.group({
+        street: [],
+        city: [],
+      })
+    );
+  }
+
+  deleteAddressGroup(index: number) {
+    const add = this.form.get('address') as FormArray;
+    add.removeAt(index);
+  }
+
+  deleteEmpty() {
+    let indexToRemove = [];
+    let fromArray = this.form.get('address') as FormArray;
+
+    fromArray.controls.forEach((control, index) => {
+      if (!control.value.street) {
+        indexToRemove.push(index);
+      }
+    });
+    indexToRemove.reverse().forEach((index) => {
+      fromArray.removeAt(index);
+    });
+  }
+}
